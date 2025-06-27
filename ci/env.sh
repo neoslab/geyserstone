d9a0b79b48375c339aa123d43519f14535d96170
neoslab <github@neoslab.com>
2025-06-27 00:23:26 -0500

    Updated project via CLI

--- ci/env.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-plugin_name=yellowstone-grpc-geyser
-plugin_lib_name=yellowstone_grpc_geyser
--- ci/solana-version.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Prints the Solana version.
-
-set -e
-cargo metadata --format-version 1 | jq -r '.packages[] | select(.name=="solana-geyser-plugin-interface") | .version'
